36d5f567ee29dd5b6f0702ec80ed06c135355b78
Horace Smith <horacesmith26@gmail.com>
2022-11-30 13:36:23 -0600

    making new test

--- code.js
+++ code.js
@@ -1,10 +1,23 @@
 // helloWorld function 
 function helloWorld() {
-    return "Hello, World!";
+    return "Hello, World!"
 }
 function sayHello(input){ return "Hello, "+ "Pat" + "!";
 }
-function isFive(input){
-    input
+function isFive(input) {
+if (input==5){
+    return true
+    } else{
+    return false
     }
+}
+function isEven(input){
+    if(input % 2 == 0){
+        return true
+    }if(input===true||false){
+        return false
+    } else{
+        return false
+    }
+}
 
--- tests.js
+++ tests.js
@@ -31,7 +31,43 @@ describe ('isFive', function(){
     it('Function only return a Boolean no matter input', function(){
         expect(typeof isFive()).toBe('boolean');
     });
-    it('should return "5" true', function(){
-        expect(typeof isFive("5").toBe("Boolean"))
+    it('should return 5 true', function(){
+        expect(isFive(5)).toBe(true)
+    })
+    it ('should return "5" as true', function(){
+        expect(isFive("5")).toBe(true)
     })
 });
+describe ('isEven', function(){
+    it('should be defined as a function', function() {
+        expect(typeof isEven).toBe('function')
+    })
+    it('returns a boolean no matter the input', function(){
+        expect(typeof isEven()).toBe('boolean')
+    })
+    it('returns true when executed with isEven(2)', function(){
+        expect(isEven(2)).toBe(true)
+    })
+    it('returns true when executed with isEven(-4)', function(){
+        expect(isEven(-4)).toBe(true)
+    })
+    it('returns false when executed with isEven(3)', function(){
+        expect(isEven(3)).toBe(false)
+    })
+    it('returns false when called with isEven("banana")', function(){
+        expect(isEven("banana")).toBe(false)
+    })
+    it('returns true when called with isEven("8")',function(){
+        expect(isEven("8")).toBe(true)
+    })
+    it('returns false when called with isEven(Infinity)', function(){
+        expect(isEven(Infinity)).toBe(false)
+    })
+    it('return false when called with a boolean input like isEven(true) or isEven(false)', function(){
+        expect(isEven(true)) .toBe(false)
+        expect(isEven(false)).toBe(false)
+    })
+    it('returns false when called without an argument like isEven()', function(){
+        expect(isEven()).toBe(false)
+    })
+    })
